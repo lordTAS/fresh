9c189777b23e597d9ab2b61c161a6308eaf8fd63
Samuel Abels <knipknap@gmail.com>
2010-07-22 15:47:40 +0200

    Add packaging infrastructure.

---
+++ setup.py
@@ -0,0 +1,40 @@
+import sys, os
+from setuptools import setup, find_packages
+from version    import __version__
+
+# Import the project description from the README.
+readme = open('README').read()
+start  = readme.index('\n\n')
+end    = readme.index('\n\n=')
+descr  = readme[start:end].strip()
+
+# Run the setup.
+setup(name             = 'Fresh',
+      version          = __version__,
+      description      = 'An automatic network inventory system',
+      long_description = descr,
+      author           = 'Samuel Abels',
+      author_email     = 'knipknap@gmail.com',
+      license          = 'GPLv2',
+      packages         = [p for p in find_packages('.')],
+      scripts          = ['fresh'],
+      install_requires = [],
+      keywords         = ' '.join(['fresh',
+                                   'inventory',
+                                   'exscript',
+                                   'telnet',
+                                   'ssh',
+                                   'network',
+                                   'networking',
+                                   'automate',
+                                   'automation',
+                                   'library']),
+      url              = 'http://github.com/knipknap/fresh/',
+      classifiers      = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Networking :: Monitoring'
+      ])
