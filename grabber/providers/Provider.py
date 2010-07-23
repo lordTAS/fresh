3b0cd4d97cc639a69905c388254c7c615707fd52
Samuel Abels <knipknap@gmail.com>
2010-07-23 12:45:28 +0200

    grabber: make auto_authorize configurable.

--- grabber/providers/Provider.py
+++ grabber/providers/Provider.py
@@ -8,6 +8,22 @@ class PostProcess(object):
     def do(self, conn):
         self.processor.start(conn, **self.args)
 
+class Authenticate(object):
+    def __init__(self, provider, xml):
+        wait      = xml.get('wait')
+        self.wait = wait is None and True or bool(wait)
+
+    def do(self, conn):
+        conn.authenticate(wait = self.wait)
+
+class AutoAuthorize(object):
+    def __init__(self, provider, xml):
+        wait      = xml.get('wait')
+        self.wait = wait is None and True or bool(wait)
+
+    def do(self, conn):
+        conn.auto_authorize(wait = self.wait)
+
 class Execute(object):
     def __init__(self, provider, xml):
         self.command  = xml.get('command')
@@ -28,8 +44,13 @@ class Provider(object):
         self.processors = processors
         self.tasks      = []
 
-        for element in xml.iterfind('execute'):
-            self.tasks.append(Execute(self, element))
+        for element in xml:
+            if element.tag == 'authenticate':
+                self.tasks.append(Authenticate(self, element))
+            elif element.tag == 'auto_authorize':
+                self.tasks.append(AutoAuthorize(self, element))
+            elif element.tag == 'execute':
+                self.tasks.append(Execute(self, element))
 
     def start(self, conn):
         for task in self.tasks:
