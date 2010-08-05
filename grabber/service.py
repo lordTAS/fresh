04ad55a93d88783a0cb815016a83b648dfe7b48c
Samuel Abels <knipknap@gmail.com>
2010-08-05 15:37:51 +0200

    grabber: query config file location from the exscript server.

--- grabber/service.py
+++ grabber/service.py
@@ -2,9 +2,8 @@ import os, sys
 sys.path.insert(0, os.path.dirname(__file__))
 from Config import Config
 
-filename = os.path.join(os.path.dirname(__file__), 'config.xml')
-config   = Config(filename)
-grabber  = config.get_grabber()
+config  = Config(__service__.config_file('config.xml'))
+grabber = config.get_grabber()
 
 def run(service, order, conn):
     grabber.grab(conn)
