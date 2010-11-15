369765ac98f24b030d04dba33ffc0f6cf094b59f
Samuel Abels <knipknap@gmail.com>
2010-11-15 09:56:55 +0100

    fix: version number import in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import sys, os
 from setuptools import setup, find_packages
-from version    import __version__
+
+# Import the file that contains the version number.
+fresh_dir = os.path.join(os.path.dirname(__file__), 'src', 'fresh')
+sys.path.insert(0, fresh_dir)
+from version import __version__
 
 # Import the project description from the README.
 readme = open('README').read()
