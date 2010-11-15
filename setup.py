7f7b0b99f909c3dbe72b0245f8650cf0dcfe7964
Samuel Abels <knipknap@gmail.com>
2010-11-15 13:08:55 +0100

    setup.py: install package data.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import sys, os
 from setuptools import setup, find_packages
+pkg = 'fresh'
 
 # Import the file that contains the version number.
-fresh_dir = os.path.join(os.path.dirname(__file__), 'src', 'fresh')
+fresh_dir    = os.path.join(os.path.dirname(__file__), 'src', pkg)
+provider_dir = os.path.join('grabber', 'providers')
 sys.path.insert(0, fresh_dir)
 from version import __version__
 
@@ -13,14 +15,24 @@ end    = readme.index('\n\n=')
 descr  = readme[start:end].strip()
 
 # Run the setup.
-setup(name             = 'Fresh',
+setup(name             = pkg,
       version          = __version__,
       description      = 'An automatic network inventory system',
       long_description = descr,
       author           = 'Samuel Abels',
       author_email     = 'knipknap@gmail.com',
       license          = 'GPLv2',
-      package_dir      = {'fresh': os.path.join('src', 'fresh')},
+      package_dir      = {pkg: os.path.join('src', pkg)},
+      package_data     = {pkg: [
+                            os.path.join('grabber', 'xsl', '*.xsl'),
+                            os.path.join('grabber', 'xsl', 'model.xsd'),
+                            os.path.join(provider_dir, 'ios', 'gel', '*.gel'),
+                            os.path.join(provider_dir, 'ios', 'xsl', '*.xsl'),
+                            os.path.join(provider_dir, 'ios_xr', 'gel', '*.gel'),
+                            os.path.join(provider_dir, 'ios_xr', 'xsl', '*.xsl'),
+                            os.path.join(provider_dir, 'junos', 'gel', '*.gel'),
+                            os.path.join(provider_dir, 'junos', 'xsl', '*.xsl'),
+                         ]},
       packages         = find_packages('src'),
       scripts          = [],
       install_requires = ['Exscript',
