34f173943b86fa381758e1f22b3f329e83268cd5
Samuel Abels <knipknap@gmail.com>
2012-03-21 16:27:26 +0100

    fix last commit.

--- src/fresh/grabber/Grabber.py
+++ src/fresh/grabber/Grabber.py
@@ -13,6 +13,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 import time
+from Exscript.protocols.Exception import LoginFailure
 from itertools import chain
 
 class Grabber(object):
